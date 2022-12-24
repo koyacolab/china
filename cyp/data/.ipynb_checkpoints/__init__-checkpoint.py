1680fb0085a2b4e78b4a51347923de6affe4fa64
Гмфкщм Тшлщдфн <uvarovn@gmail.com>
2022-12-24 20:52:14 +0800

    add feature_engineering3.py

--- cyp/data/.ipynb_checkpoints/__init__-checkpoint.py
+++ cyp/data/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,4 +2,3 @@ from .exporting import MODISExporter
 from .preprocessing import DataCleaner
 from .feature_engineering import Engineer
 from .feature_engineering2 import Engineer2
-from .feature_engineering3 import Engineer3
