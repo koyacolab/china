d99370939f7d82de59c29c0c0f562d9c8e4de3e7
Uvarov Nikolay <uvarovn@gmail.com>
2023-01-13 01:27:58 +0800

    class experiments

--- ATFTClass.py
+++ ATFTClass.py
@@ -55,21 +55,30 @@ class ModelBase:
     def __init__(self, 
                  home_dir = '/hy-tmp',
                  datasetfile = 'corn_china_pandas_onebands.csv',
+                 # datasetfile = 'corn_china_pandas_onebands.csv',
                  predicted_year = 2010,
                  batch_size = 1, 
                  encoder_length = 20,
                  save_checkpoint = False,
+                 save_checkpoint_model = 'best-model',
                  exp_name = '',
+                 crop_name = '',
                 ):
     
         self.home_dir = '/hy-tmp'
-        # os.chdir(home_dir)
+        os.chdir(home_dir)
+        
+        print(exp_name, crop_name)
         
         if len(exp_name) == 0:
             print("exp_name is not definite")
             sys.exit(0)
+        if len(crop_name) == 0:
+            print("crop_name is not definite")
+            sys.exit(0)
             
         self.exp_name = exp_name
+        self.crop_name = crop_name
         self.batch_size = batch_size
         self.predicted_year = str(predicted_year)
         
@@ -80,10 +89,12 @@ class ModelBase:
         self.logger_comment = f"encoder_length={encoder_length}"
         
         self.save_checkpoint = save_checkpoint
+        self.save_checkpoint_model = save_checkpoint_model
 
         freeze_support()
         warnings.filterwarnings("ignore")
 
+        print(f'{crop_name}_{exp_name} loading {datasetfile}')
         data = pd.read_csv(os.path.join(home_dir, datasetfile))  # encoding= 'unicode_escape')
 
         data['county'] = data['county'].astype(int)
@@ -105,14 +116,8 @@ class ModelBase:
             mean_sownareas.extend(mean_sownarea.round(2) * np.ones((len(X),1)))
             mean_yieldvals.extend(mean_yieldval.round(2) * np.ones((len(X),1)))
 
-
         data.insert(4, "mean_areas", np.asarray(mean_sownareas).astype(np.float64))  
         data.insert(6, "mean_yields", np.asarray(mean_yieldvals).astype(np.float64)) 
-
-        # mean_sownareas = []
-        # mean_yieldvals = []
-
-        # data
 
         data['years'] = data['years'].astype(str)
         data['county'] = data['county'].astype(str)
@@ -169,8 +174,9 @@ class ModelBase:
         # convert datasets to dataloaders for training
         self.train_dataloader = train_dataset_with_covariates.to_dataloader(train=True,  batch_size=self.batch_size, num_workers=4)
         self.valid_dataloader = valid_dataset_with_covariates.to_dataloader(train=False, batch_size=self.batch_size, num_workers=4)
-
-        # exp_name = f"cycle_{predicted_year}"  
+        
+        self.actuals = torch.cat([y[0] for x, y in iter(self.valid_dataloader)])
+        self.predictions = self.actuals
 
         checkpoint_callback = ModelCheckpoint(dirpath='/hy-tmp/chck/'+self.logger_name, every_n_epochs=1)
 
@@ -178,7 +184,6 @@ class ModelBase:
 
         lr_monitor = LearningRateMonitor(logging_interval='epoch')
 
-        # trainer = Trainer(gpus=1, max_epochs=100, limit_train_batches=2606, logger=logger)
         self.trainer = Trainer(accelerator='gpu', devices="0, 1", logger=logger, max_epochs=30, 
                           log_every_n_steps=1, callbacks=[checkpoint_callback, lr_monitor], fast_dev_run=True)
 
@@ -199,46 +204,59 @@ class ModelBase:
         # reduce_on_plateau_patience=4,
         )
         
-        self.best_tft = []
-        self.checkpoint = f"model_{self.exp_name}.ckpt"
+        self.best_tft = self.model
+        self.checkpoint = f"{self.crop_name}_{self.exp_name}.ckpt"
         
     def train(self,):
         print('Train(): learning_rate', self.model.hparams.learning_rate)
-        self.trainer.fit(self.model, train_dataloaders=self.train_dataloader, val_dataloaders=self.valid_dataloader)
-        if self.save_checkpoint == True:
-            self.trainer.save_checkpoint(self.checkpoint)
-            self.best_tft = TemporalFusionTransformer.load_from_checkpoint(self.checkpoint)
+        self.trainer = self.trainer.fit(self.model, train_dataloaders=self.train_dataloader, val_dataloaders=self.valid_dataloader)
+        self.best_tft = self.model
+        # if self.save_checkpoint_model == 'best-model':
+        #     print(f"{self.crop_name} {self.save_checkpoint} best-model loading...")
+        #     best_model_path = self.trainer.checkpoint_callback.best_model_path
+        #     print('best_model_path:', type(best_model_path), best_model_path)
+        #     self.best_tft = TemporalFusionTransformer.load_from_checkpoint(self.trainer.checkpoint_callback.best_model_path)
+        #     print(f"{self.crop_name} {self.save_checkpoint} best-model loaded...")
+        #     if self.save_checkpoint == True:
+        #         self.best_tft.save_checkpoint(self.checkpoint)
+        #         print(f"{self.crop_name} {self.save_checkpoint} best-model saved...")
+        # elif self.save_checkpoint_model == 'last-model':
+        #     self.trainer.save_checkpoint(self.checkpoint)
+        #     self.best_tft = TemporalFusionTransformer.load_from_checkpoint(self.checkpoint)
+        #     print(f"{self.crop_name} {self.save_checkpoint} last-model loaded...")
         
     def predict(self,):
         # calcualte mean absolute error on validation set
-        actuals = torch.cat([y[0] for x, y in iter(valid_dataloader)])
-        predictions = best_tft.predict(valid_dataloader)
-        (actuals - predictions).abs().mean()
+        # actuals = self.actuals     # torch.cat([y[0] for x, y in iter(self.valid_dataloader)])
+        self.predictions = self.best_tft.predict(self.valid_dataloader)
+        (self.actuals - self.predictions).abs().mean()
+        print('self.actuals type is:', type(self.actuals))     
         
-        X = [X for X in range(0, actuals.shape[0])]
+    def plot_predict(self,):
+        
+        X = [X for X in range(0, self.actuals.shape[0])]
 
         fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
 
-        ax1.plot(X, actuals, color='b', label="Actual")
-        ax1.plot(X, predictions, color='r', label="Predicted")
-        ax1.set_title(logger_name)
+        ax1.plot(X, self.actuals, color='b', label="Actual")
+        ax1.plot(X, self.predictions, color='r', label="Predicted")
+        ax1.set_title(self.logger_name)
 
-        files = os.path.join(home_dir, f'TFT_{self.predicted_year}_{self.exp_name}.png')
+        files = os.path.join(self.home_dir, f'TFTC_{self.crop_name}_{self.predicted_year}_{self.exp_name}.png')
         plt.savefig(files, bbox_inches='tight')
         # plt.show()
     
-        X = [X for X in range(0, actuals.shape[0])]
         X = [X for X in range(1, 21)]
 
         fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
 
-        outs = int( actuals.shape[0] / 20 )
+        outs = int( self.actuals.shape[0] / 20 )
 
         act = []
         pred = []
         for ii in range(0,outs*20,outs):
-            act.append(actuals[ii:ii+outs].mean())
-            pred.append(predictions[ii:ii+outs].mean())
+            act.append(self.actuals[ii:ii+outs].mean())
+            pred.append(self.predictions[ii:ii+outs].mean())
 
         ax1.plot(X, np.asarray(act), 'bo', label="Actual")
         ax1.plot(X, np.asarray(pred), 'ro', label="Predicted")
@@ -249,7 +267,7 @@ class ModelBase:
         plt.ylabel("Yield")
         ax1.set_title(f"Corn yield predictions for {self.predicted_year} with Temporal Fusion Transformer")
 
-        files = os.path.join(home_dir, f'TFT_{self.predicted_year}_corn_yield_{self.exp_name}.png')
+        files = os.path.join(self.home_dir, f'TFT_{self.crop_name}_{self.predicted_year}_yield_{self.exp_name}.png')
         plt.savefig(files, bbox_inches='tight')
         # plt.show()
 
@@ -261,8 +279,8 @@ class ModelBase:
         act = []
         pred = []
         for ii in range(0,outs*20,outs):
-            act.append(actuals[ii:ii+outs].mean())
-            pred.append(predictions[ii:ii+outs].mean())
+            act.append(self.actuals[ii:ii+outs].mean())
+            pred.append(self.predictions[ii:ii+outs].mean())
 
         ax1.plot(X, (1-np.abs(np.asarray(act)-np.asarray(pred))) * 100, 'bo', label="Actual")
         # ax1.plot(X, np.asarray(pred), 'r.', label="Predicted")
@@ -272,16 +290,17 @@ class ModelBase:
         plt.ylabel("Yild Accuracy")
         ax1.set_title(f"ACCURACY for Temporal Fusion Transformer for {self.predicted_year} year for corn yield predict") 
 
-        files = os.path.join(home_dir, f'TFT_{self.predicted_year}_corn_accuracy_{self.exp_name}.png')
+        files = os.path.join(self.home_dir, f'TFT_{self.crop_name}_{self.predicted_year}_accuracy_{self.exp_name}.png')
         plt.savefig(files, bbox_inches='tight')
         
         
 class RunTask:
     @staticmethod
-    def train_TFT():
-        model = ModelBase()
+    def train_TFT(exp_name, crop_name):
+        model = ModelBase(exp_name=exp_name, crop_name=crop_name)
         model.train()
         model.predict()
+        model.plot_predict()
 
 if __name__ == "__main__":
     
@@ -292,83 +311,3 @@ if __name__ == "__main__":
     
     # main()
     
-
-
-
-#     # load the best model according to the validation loss
-#     # (given that we use early stopping, this is not necessarily the last epoch)
-#     best_model_path = trainer.checkpoint_callback.best_model_path
-#     best_tft = TemporalFusionTransformer.load_from_checkpoint(best_model_path)
-#     # trainer.save_checkpoint(f"tft1_best_model_{exp_name}.ckpt")
-#     # best_tft = TemporalFusionTransformer.load_from_checkpoint(f"tft1_best_model_{exp_name}.ckpt")
-
-#     # calcualte mean absolute error on validation set
-#     actuals = torch.cat([y[0] for x, y in iter(valid_dataloader)])
-#     predictions = best_tft.predict(valid_dataloader)
-#     (actuals - predictions).abs().mean()
-
-#     print(type(actuals), actuals.shape, type(predictions), predictions.shape)
-#     # print(actuals, predictions)
-
-#     X = [X for X in range(0, actuals.shape[0])]
-
-#     fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
-
-#     ax1.plot(X, actuals, color='b', label="Actual")
-#     ax1.plot(X, predictions, color='r', label="Predicted")
-#     ax1.set_title(logger_name)
-
-#     files = os.path.join(home_dir, f'TFT{batch_size}_{exp_name}.png')
-#     plt.savefig(files, bbox_inches='tight')
-#     plt.show()
-
-#     X = [X for X in range(0, actuals.shape[0])]
-#     X = [X for X in range(1, 21)]
-
-#     fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
-
-#     outs = int( actuals.shape[0] / 20 )
-
-#     act = []
-#     pred = []
-#     for ii in range(0,outs*20,outs):
-#         act.append(actuals[ii:ii+outs].mean())
-#         pred.append(predictions[ii:ii+outs].mean())
-
-#     ax1.plot(X, np.asarray(act), 'bo', label="Actual")
-#     ax1.plot(X, np.asarray(pred), 'ro', label="Predicted")
-#     leg = plt.legend(loc='upper center')
-#     plt.xticks(X)
-#     ax1.set_ylim([0, 1])
-#     plt.xlabel("counties")
-#     plt.ylabel("Yield")
-#     ax1.set_title("Corn yield predictions for 2018 with Temporal Fusion Transformer")
-
-#     files = os.path.join(home_dir, f'TFT{batch_size}_corn_yield_{exp_name}.png')
-#     plt.savefig(files, bbox_inches='tight')
-#     plt.show()
-
-#     X = [X for X in range(0, actuals.shape[0])]
-#     X = [X for X in range(1, 21)]
-
-#     fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
-
-#     act = []
-#     pred = []
-#     for ii in range(0,outs*20,outs):
-#         act.append(actuals[ii:ii+outs].mean())
-#         pred.append(predictions[ii:ii+outs].mean())
-
-#     ax1.plot(X, (1-np.abs(np.asarray(act)-np.asarray(pred))) * 100, 'bo', label="Actual")
-#     # ax1.plot(X, np.asarray(pred), 'r.', label="Predicted")
-#     ax1.set_ylim([70, 105])
-#     plt.xticks(X)
-#     plt.xlabel("counties")
-#     plt.ylabel("Yild Accuracy")
-#     ax1.set_title("ACCURACY for Temporal Fusion Transformer for 2018 year for corn yield predict") # + logger_name)
-
-#     files = os.path.join(home_dir, f'TFT{batch_size}_corn_accuracy_{exp_name}.png')
-#     plt.savefig(files, bbox_inches='tight')
-#     plt.show()
-
-
